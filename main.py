4f85e05619559aaa7b060b422d966086c3b78318
ika-100 <621.pono@gmail.com>
2026-01-15 23:17:56 +0900

    チャット画面の作成

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 import random
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],   # ← OPTIONS を含める
+    allow_headers=["*"],
+)
+
 
 user_memory = {}
 
